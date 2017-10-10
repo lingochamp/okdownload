0a74c66781f3529b7711fe270a6a8acee2fd2f34
Jacksgong <igzhenjie@gmail.com>
2017-10-10 13:31:43 +0800

    demo: remove obtain task on demo because okdownload not ready yet

--- app-okdownloader/src/main/java/cn/dreamtobe/app/okdownloader/MainActivity.kt
+++ app-okdownloader/src/main/java/cn/dreamtobe/app/okdownloader/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import cn.dreamtobe.app.okdownloader.R
-import cn.dreamtobe.okdownload.DownloadListener
-import cn.dreamtobe.okdownload.OkDownload
-import cn.dreamtobe.okdownload.core.breakpoint.BreakpointInfo
-import cn.dreamtobe.okdownload.core.connection.DownloadConnection
-import cn.dreamtobe.okdownload.DownloadTask
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,49 +31,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
-        OkDownload.obtainTask("", application.cacheDir)
-                .enqueue(object : DownloadListener {
-                    override fun connectStart(task: DownloadTask?, blockIndex: Int) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun connectEnd(task: DownloadTask?, blockIndex: Int, connection: DownloadConnection?, connected: DownloadConnection.Connected?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun taskStart(task: DownloadTask?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun breakpointData(task: DownloadTask?, info: BreakpointInfo?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun downloadFromBeginning(task: DownloadTask?, info: BreakpointInfo?, cause: DownloadListener.ResumeFailedCause?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun downloadFromBreakpoint(task: DownloadTask?, info: BreakpointInfo?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun fetchStart(task: DownloadTask?, blockIndex: Int, contentLength: Long) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun fetchProgress(task: DownloadTask?, blockIndex: Int, fetchedBytes: Long) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun fetchEnd(task: DownloadTask?, blockIndex: Int, contentLength: Long) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                    override fun taskEnd(task: DownloadTask?, cause: DownloadListener.EndCause?, realCause: Exception?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                })
     }
 }
