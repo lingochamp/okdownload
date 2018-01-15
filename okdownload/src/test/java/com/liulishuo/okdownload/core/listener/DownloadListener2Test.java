aa3b61c305b23213d4a10ae842bbb0d394d6582f
Jacksgong <igzhenjie@gmail.com>
2018-01-15 10:57:24 +0800

    tests: add unit-test for listener2, this test is just for codecov, because the origin-class is nothing need to be tested

---
+++ okdownload/src/test/java/com/liulishuo/okdownload/core/listener/DownloadListener2Test.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017 LingoChamp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liulishuo.okdownload.core.listener;
+
+import android.support.annotation.Nullable;
+
+import com.liulishuo.okdownload.DownloadTask;
+import com.liulishuo.okdownload.core.breakpoint.BreakpointInfo;
+import com.liulishuo.okdownload.core.cause.EndCause;
+import com.liulishuo.okdownload.core.cause.ResumeFailedCause;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DownloadListener2Test {
+
+    private DownloadListener2 listener2 = new DownloadListener2() {
+        @Override public void taskStart(DownloadTask task) {
+        }
+
+        @Override
+        public void taskEnd(DownloadTask task, EndCause cause, @Nullable Exception realCause) {
+        }
+    };
+
+    @Mock private DownloadTask task;
+    @Mock private BreakpointInfo info;
+    @Mock private ResumeFailedCause cause;
+    @Mock private Map<String, List<String>> headerFields;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void downloadFromBeginning() throws Exception {
+        listener2.downloadFromBeginning(task, info, cause);
+    }
+
+    @Test
+    public void downloadFromBreakpoint() throws Exception {
+        listener2.downloadFromBreakpoint(task, info);
+    }
+
+    @Test
+    public void connectStart() throws Exception {
+        listener2.connectStart(task, 1, headerFields);
+    }
+
+    @Test
+    public void connectEnd() throws Exception {
+        listener2.connectEnd(task, 1, 200, headerFields);
+    }
+
+    @Test
+    public void splitBlockEnd() throws Exception {
+        listener2.splitBlockEnd(task, info);
+    }
+
+    @Test
+    public void fetchStart() throws Exception {
+        listener2.fetchStart(task, 1, 2);
+    }
+
+    @Test
+    public void fetchProgress() throws Exception {
+        listener2.fetchProgress(task, 1, 2);
+    }
+
+    @Test
+    public void fetchEnd() throws Exception {
+        listener2.fetchEnd(task, 1, 2);
+    }
+
+}
