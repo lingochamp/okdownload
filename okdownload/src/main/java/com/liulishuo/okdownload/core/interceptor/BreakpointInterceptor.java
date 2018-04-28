c23aef3d40d76f9d7c20ce91bbda56b95b847cad
Jacksgong <jacks@liulishuo.com>
2018-04-28 23:03:38 +0800

    feat: cover the case of resource content-length is changed between block-connection and trial-connection for single-block task closes #31

--- okdownload/src/main/java/com/liulishuo/okdownload/core/interceptor/BreakpointInterceptor.java
+++ okdownload/src/main/java/com/liulishuo/okdownload/core/interceptor/BreakpointInterceptor.java
@@ -16,20 +16,32 @@
 
 package com.liulishuo.okdownload.core.interceptor;
 
+import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 
+import com.liulishuo.okdownload.OkDownload;
+import com.liulishuo.okdownload.core.Util;
+import com.liulishuo.okdownload.core.breakpoint.BlockInfo;
 import com.liulishuo.okdownload.core.breakpoint.BreakpointInfo;
 import com.liulishuo.okdownload.core.breakpoint.DownloadStore;
 import com.liulishuo.okdownload.core.connection.DownloadConnection;
 import com.liulishuo.okdownload.core.download.DownloadChain;
 import com.liulishuo.okdownload.core.exception.InterruptException;
+import com.liulishuo.okdownload.core.exception.RetryException;
 import com.liulishuo.okdownload.core.file.MultiPointOutputStream;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.liulishuo.okdownload.core.Util.CHUNKED_CONTENT_LENGTH;
+import static com.liulishuo.okdownload.core.Util.CONTENT_LENGTH;
+import static com.liulishuo.okdownload.core.Util.CONTENT_RANGE;
+import static com.liulishuo.okdownload.core.cause.ResumeFailedCause.CONTENT_LENGTH_CHANGED;
 
 public class BreakpointInterceptor implements Interceptor.Connect, Interceptor.Fetch {
+
+    private static final String TAG = "BreakpointInterceptor";
 
     @NonNull @Override
     public DownloadConnection.Connected interceptConnect(DownloadChain chain) throws IOException {
@@ -38,6 +50,33 @@ public class BreakpointInterceptor implements Interceptor.Connect, Interceptor.F
 
         if (chain.getCache().isInterrupt()) {
             throw InterruptException.SIGNAL;
+        }
+
+        if (info.getBlockCount() == 1 && !info.isChunked()) {
+            // only one block to download this resource
+            // use this block response header instead of trial result if they are different.
+            final long blockInstanceLength = getExactContentLengthRangeFrom0(connected);
+            final long infoInstanceLength = info.getTotalLength();
+            if (blockInstanceLength > 0 && blockInstanceLength != infoInstanceLength) {
+                Util.d(TAG, "SingleBlock special check: the response instance-length["
+                        + blockInstanceLength + "] isn't equal to the instance length from trial-"
+                        + "connection[" + infoInstanceLength + "]");
+                final BlockInfo blockInfo = info.getBlock(0);
+                boolean isFromBreakpoint = blockInfo.getRangeLeft() != 0;
+
+                final BlockInfo newBlockInfo = new BlockInfo(0, blockInstanceLength);
+                info.resetBlockInfos();
+                info.addBlock(newBlockInfo);
+
+                if (isFromBreakpoint) {
+                    final String msg = "Discard breakpoint because of on this special case, we have"
+                            + " to download from beginning";
+                    Util.w(TAG, msg);
+                    throw new RetryException(msg);
+                }
+                OkDownload.with().callbackDispatcher().dispatch()
+                        .downloadFromBeginning(chain.getTask(), info, CONTENT_LENGTH_CHANGED);
+            }
         }
 
         // update for connected.
@@ -93,4 +132,40 @@ public class BreakpointInterceptor implements Interceptor.Connect, Interceptor.F
         return fetchLength;
     }
 
+    private static final Pattern CONTENT_RANGE_RIGHT_VALUE = Pattern
+            .compile(".*\\d+ *- *(\\d+) */ *\\d+");
+
+    /**
+     * Get the exactly content-length, on this method we assume the range is from 0.
+     */
+    @IntRange(from = -1)
+    long getExactContentLengthRangeFrom0(@NonNull DownloadConnection.Connected connected) {
+        final String contentRangeField = connected.getResponseHeaderField(CONTENT_RANGE);
+        long contentLength = -1;
+        if (!Util.isEmpty(contentRangeField)) {
+            final long rightRange = getRangeRightFromContentRange(contentRangeField);
+            // for the range from 0, the contentLength is just right-range +1.
+            if (rightRange > 0) contentLength = rightRange + 1;
+        }
+
+        if (contentLength < 0) {
+            // content-length
+            final String contentLengthField = connected.getResponseHeaderField(CONTENT_LENGTH);
+            if (!Util.isEmpty(contentLengthField)) {
+                contentLength = Long.valueOf(contentLengthField);
+            }
+        }
+
+        return contentLength;
+    }
+
+    @IntRange(from = -1)
+    static long getRangeRightFromContentRange(@NonNull String contentRange) {
+        Matcher m = CONTENT_RANGE_RIGHT_VALUE.matcher(contentRange);
+        if (m.find()) {
+            return Long.valueOf(m.group(1));
+        }
+
+        return -1;
+    }
 }
--- okdownload/src/test/java/com/liulishuo/okdownload/core/interceptor/BreakpointInterceptorTest.java
+++ okdownload/src/test/java/com/liulishuo/okdownload/core/interceptor/BreakpointInterceptorTest.java
@@ -16,17 +16,23 @@
 
 package com.liulishuo.okdownload.core.interceptor;
 
-import com.liulishuo.okdownload.core.Util;
+import com.liulishuo.okdownload.DownloadListener;
+import com.liulishuo.okdownload.DownloadTask;
+import com.liulishuo.okdownload.OkDownload;
 import com.liulishuo.okdownload.core.breakpoint.BlockInfo;
 import com.liulishuo.okdownload.core.breakpoint.BreakpointInfo;
 import com.liulishuo.okdownload.core.breakpoint.DownloadStore;
+import com.liulishuo.okdownload.core.connection.DownloadConnection;
+import com.liulishuo.okdownload.core.download.DownloadCache;
 import com.liulishuo.okdownload.core.download.DownloadChain;
+import com.liulishuo.okdownload.core.exception.RetryException;
 import com.liulishuo.okdownload.core.file.MultiPointOutputStream;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import java.io.File;
@@ -35,10 +41,16 @@ import java.io.IOException;
 import static com.liulishuo.okdownload.TestUtils.mockDownloadChain;
 import static com.liulishuo.okdownload.TestUtils.mockOkDownload;
 import static com.liulishuo.okdownload.core.Util.CHUNKED_CONTENT_LENGTH;
+import static com.liulishuo.okdownload.core.Util.CONTENT_LENGTH;
+import static com.liulishuo.okdownload.core.Util.CONTENT_RANGE;
+import static com.liulishuo.okdownload.core.cause.ResumeFailedCause.CONTENT_LENGTH_CHANGED;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -49,25 +61,30 @@ public class BreakpointInterceptorTest {
 
     private BreakpointInterceptor interceptor;
 
-    @Mock private BreakpointInfo mockInfo;
+    @Mock private BreakpointInfo info;
     @Mock private DownloadStore store;
+    @Mock private DownloadConnection.Connected connected;
+    @Mock private DownloadCache cache;
+    @Mock private BlockInfo blockInfo;
+    @Mock private DownloadTask task;
 
-    private DownloadChain mockChain;
+    private DownloadChain chain;
 
     private final String existPath = "./exist-path";
 
     @BeforeClass
     public static void setupClass() throws IOException {
         mockOkDownload();
-        Util.setLogger(mock(Util.Logger.class));
     }
 
     @Before
     public void setup() throws IOException {
         initMocks(this);
 
-        mockChain = mockDownloadChain();
-        when(mockChain.getDownloadStore()).thenReturn(store);
+        chain = mockDownloadChain();
+        when(chain.getDownloadStore()).thenReturn(store);
+        when(store.update(info)).thenReturn(true);
+        when(chain.getTask()).thenReturn(task);
 
         interceptor = spy(new BreakpointInterceptor());
         new File(existPath).createNewFile();
@@ -82,27 +99,27 @@ public class BreakpointInterceptorTest {
     public void interceptConnect_process() throws IOException {
         when(store.update(any(BreakpointInfo.class))).thenReturn(true);
 
-        interceptor.interceptConnect(mockChain);
+        interceptor.interceptConnect(chain);
 
-        verify(store).update(mockChain.getInfo());
-        verify(mockChain).processConnect();
+        verify(store).update(chain.getInfo());
+        verify(chain).processConnect();
     }
 
     @Test
     public void interceptFetch_finish() throws IOException {
         BlockInfo blockInfo2 = new BlockInfo(10, 20);
 
-        when(mockChain.getResponseContentLength()).thenReturn(new Long(10));
-        when(mockChain.getInfo()).thenReturn(mockInfo);
-        when(mockChain.getOutputStream()).thenReturn(mock(MultiPointOutputStream.class));
-        when(mockChain.getBlockIndex()).thenReturn(1);
-        when(mockInfo.getBlock(1)).thenReturn(blockInfo2);
-        when(mockInfo.getBlockCount()).thenReturn(2);
-        when(mockChain.loopFetch()).thenReturn(1L, 1L, 2L, 1L, 5L, -1L);
+        when(chain.getResponseContentLength()).thenReturn(new Long(10));
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getOutputStream()).thenReturn(mock(MultiPointOutputStream.class));
+        when(chain.getBlockIndex()).thenReturn(1);
+        when(info.getBlock(1)).thenReturn(blockInfo2);
+        when(info.getBlockCount()).thenReturn(2);
+        when(chain.loopFetch()).thenReturn(1L, 1L, 2L, 1L, 5L, -1L);
 
-        final long contentLength = interceptor.interceptFetch(mockChain);
-        verify(mockChain, times(6)).loopFetch();
-        verify(mockChain).flushNoCallbackIncreaseBytes();
+        final long contentLength = interceptor.interceptFetch(chain);
+        verify(chain, times(6)).loopFetch();
+        verify(chain).flushNoCallbackIncreaseBytes();
 
         assertThat(contentLength).isEqualTo(10);
     }
@@ -111,15 +128,129 @@ public class BreakpointInterceptorTest {
     public void interceptFetch_chunked() throws IOException {
         final BlockInfo blockInfo = new BlockInfo(0, 0);
 
-        when(mockChain.getResponseContentLength()).thenReturn(Long.valueOf(CHUNKED_CONTENT_LENGTH));
-        when(mockChain.getInfo()).thenReturn(mockInfo);
-        when(mockChain.getOutputStream()).thenReturn(mock(MultiPointOutputStream.class));
-        when(mockInfo.getBlock(anyInt())).thenReturn(blockInfo);
-        when(mockChain.loopFetch()).thenReturn(1L, 1L, 2L, 1L, 5L, -1L);
+        when(chain.getResponseContentLength()).thenReturn(Long.valueOf(CHUNKED_CONTENT_LENGTH));
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getOutputStream()).thenReturn(mock(MultiPointOutputStream.class));
+        when(info.getBlock(anyInt())).thenReturn(blockInfo);
+        when(chain.loopFetch()).thenReturn(1L, 1L, 2L, 1L, 5L, -1L);
 
-        final long contentLength = interceptor.interceptFetch(mockChain);
-        verify(mockChain, times(6)).loopFetch();
+        final long contentLength = interceptor.interceptFetch(chain);
+        verify(chain, times(6)).loopFetch();
 
         assertThat(contentLength).isEqualTo(10);
+    }
+
+    @Test
+    public void getRangeRightFromContentRange() {
+        assertThat(BreakpointInterceptor.getRangeRightFromContentRange("bytes 1-111/222"))
+                .isEqualTo(111L);
+
+        assertThat(BreakpointInterceptor.getRangeRightFromContentRange("bytes 1 -111/222"))
+                .isEqualTo(111L);
+
+        assertThat(BreakpointInterceptor.getRangeRightFromContentRange("bytes 1 - 111/222"))
+                .isEqualTo(111L);
+
+        assertThat(BreakpointInterceptor.getRangeRightFromContentRange("bytes 1 - 111 /222"))
+                .isEqualTo(111L);
+
+        assertThat(BreakpointInterceptor.getRangeRightFromContentRange("bytes 1 - 111 / 222"))
+                .isEqualTo(111L);
+    }
+
+    @Test
+    public void getExactContentLength_contentRange() {
+        when(connected.getResponseHeaderField(CONTENT_RANGE)).thenReturn("bytes 0-111/222");
+        assertThat(interceptor.getExactContentLengthRangeFrom0(connected)).isEqualTo(112L);
+    }
+
+    @Test
+    public void getExactContentLength_contentLength() {
+        when(connected.getResponseHeaderField(CONTENT_LENGTH)).thenReturn("123");
+        assertThat(interceptor.getExactContentLengthRangeFrom0(connected)).isEqualTo(123L);
+    }
+
+    @Test
+    public void interceptConnect_singleBlockCheck() throws IOException {
+        mockOkDownload();
+
+        when(chain.processConnect()).thenReturn(connected);
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getCache()).thenReturn(cache);
+        when(info.getBlock(0)).thenReturn(blockInfo);
+        when(info.getBlockCount()).thenReturn(1);
+        when(info.isChunked()).thenReturn(false);
+
+        when(info.getTotalLength()).thenReturn(1L);
+        doReturn(2L).when(interceptor).getExactContentLengthRangeFrom0(connected);
+
+        interceptor.interceptConnect(chain);
+
+        ArgumentCaptor<BlockInfo> captor = ArgumentCaptor.forClass(BlockInfo.class);
+
+        verify(info).addBlock(captor.capture());
+        verify(info).resetBlockInfos();
+
+        final BlockInfo addBlockInfo = captor.getValue();
+        assertThat(addBlockInfo.getRangeLeft()).isZero();
+        assertThat(addBlockInfo.getContentLength()).isEqualTo(2L);
+        final DownloadListener listener = OkDownload.with().callbackDispatcher().dispatch();
+        verify(listener).downloadFromBeginning(eq(task), eq(info), eq(CONTENT_LENGTH_CHANGED));
+    }
+
+    @Test(expected = RetryException.class)
+    public void interceptConnect_singleBlockCheck_fromBreakpoint() throws IOException {
+        when(chain.processConnect()).thenReturn(connected);
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getCache()).thenReturn(cache);
+        when(info.getBlock(0)).thenReturn(blockInfo);
+        when(info.getBlockCount()).thenReturn(1);
+        when(info.isChunked()).thenReturn(false);
+
+        when(info.getTotalLength()).thenReturn(1L);
+        doReturn(2L).when(interceptor).getExactContentLengthRangeFrom0(connected);
+
+        when(blockInfo.getRangeLeft()).thenReturn(1L);
+
+        interceptor.interceptConnect(chain);
+    }
+
+    @Test
+    public void interceptConnect_singleBlockCheck_expect() throws IOException {
+        when(chain.processConnect()).thenReturn(connected);
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getCache()).thenReturn(cache);
+        when(info.getBlockCount()).thenReturn(1);
+        when(info.isChunked()).thenReturn(false);
+        when(info.getBlock(0)).thenReturn(blockInfo);
+
+        when(info.getTotalLength()).thenReturn(2L);
+        doReturn(2L).when(interceptor).getExactContentLengthRangeFrom0(connected);
+
+        interceptor.interceptConnect(chain);
+
+        verify(info, never()).resetBlockInfos();
+        verify(info, never()).addBlock(any(BlockInfo.class));
+    }
+
+    @Test
+    public void interceptConnect_notSingleBlockOrChunked() throws IOException {
+        when(chain.processConnect()).thenReturn(connected);
+        when(chain.getInfo()).thenReturn(info);
+        when(chain.getCache()).thenReturn(cache);
+
+        // not single block
+        when(info.getBlockCount()).thenReturn(2);
+        when(info.isChunked()).thenReturn(false);
+        interceptor.interceptConnect(chain);
+        verify(info, never()).resetBlockInfos();
+        verify(info, never()).addBlock(any(BlockInfo.class));
+
+        // chunked
+        when(info.getBlockCount()).thenReturn(1);
+        when(info.isChunked()).thenReturn(true);
+        interceptor.interceptConnect(chain);
+        verify(info, never()).resetBlockInfos();
+        verify(info, never()).addBlock(any(BlockInfo.class));
     }
 }
