9a74fa0808f482c05907a382e687c51fa1a9fcdd
Tianhua Ran <tianhua.ran@liulishuo.com>
2019-09-27 18:49:20 +0800

    refactor: use kotlin extension function to start download in SingleActivity

--- sample/src/main/java/com/liulishuo/okdownload/sample/SingleActivity.kt
+++ sample/src/main/java/com/liulishuo/okdownload/sample/SingleActivity.kt
@@ -23,21 +23,16 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.liulishuo.okdownload.DownloadTask
-import com.liulishuo.okdownload.SpeedCalculator
 import com.liulishuo.okdownload.StatusUtil
 import com.liulishuo.okdownload.core.Util
-import com.liulishuo.okdownload.core.breakpoint.BlockInfo
-import com.liulishuo.okdownload.core.breakpoint.BreakpointInfo
 import com.liulishuo.okdownload.core.cause.EndCause
-import com.liulishuo.okdownload.core.listener.DownloadListener4WithSpeed
-import com.liulishuo.okdownload.core.listener.assist.Listener4SpeedAssistExtend
+import com.liulishuo.okdownload.kotlin.enqueue4WithSpeed
 import com.liulishuo.okdownload.sample.base.BaseSampleActivity
 import com.liulishuo.okdownload.sample.util.DemoUtil
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import java.io.FileInputStream
 import java.io.InputStream
 import java.security.MessageDigest
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
-import kotlin.experimental.and
 
 /**
  * On this demo you can see the simplest way to download a task.
@@ -62,7 +57,7 @@ class SingleActivity : BaseSampleActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if (task != null) task!!.cancel()
+        task?.cancel()
     }
 
     private fun initSingleDownload(
@@ -89,35 +84,36 @@ class SingleActivity : BaseSampleActivity() {
             .build()
     }
 
-    private fun initStatus(statusTv: TextView, progressBar: ProgressBar) {
-        val status = StatusUtil.getStatus(task!!)
+    private fun initStatus(statusTv: TextView, progressBar: ProgressBar) = task?.let {
+        val status = StatusUtil.getStatus(it)
         if (status == StatusUtil.Status.COMPLETED) {
             progressBar.progress = progressBar.max
         }
-
         statusTv.text = status.toString()
-        val info = StatusUtil.getCurrentInfo(task!!)
-        if (info != null) {
+        StatusUtil.getCurrentInfo(it)?.let { info ->
             Log.d(TAG, "init status with: $info")
             DemoUtil.calcProgressToView(progressBar, info.totalOffset, info.totalLength)
         }
     }
 
-    private fun initAction(actionView: View, actionTv: TextView, statusTv: TextView,
-                           progressBar: ProgressBar) {
+    private fun initAction(
+        actionView: View,
+        actionTv: TextView,
+        statusTv: TextView,
+        progressBar: ProgressBar
+    ) {
         actionTv.setText(R.string.start)
         actionView.setOnClickListener {
-            val started = task!!.tag != null
-
-            if (started) {
-                // to cancel
-                task!!.cancel()
-            } else {
-                actionTv.setText(R.string.cancel)
-                // to start
-                startTask(statusTv, progressBar, actionTv)
-                // mark
-                task!!.tag = "mark-task-started"
+            task?.let {
+                if (it.tag != null) {
+                    // to cancel
+                    it.cancel()
+                } else {
+                    // to start
+                    actionTv.setText(R.string.cancel)
+                    startTask(statusTv, progressBar, actionTv)
+                    it.tag = "mark-task-started"
+                }
             }
         }
     }
@@ -125,96 +121,46 @@ class SingleActivity : BaseSampleActivity() {
     private fun startTask(
         statusTv: TextView,
         progressBar: ProgressBar,
-        actionTv: TextView) {
-
-        task!!.enqueue(object : DownloadListener4WithSpeed() {
-            private var totalLength: Long = 0
-            private var readableTotalLength: String? = null
-
-            override fun taskStart(task: DownloadTask) {
-                statusTv.setText(R.string.task_start)
-            }
-
-            override fun infoReady(
-                task: DownloadTask,
-                info: BreakpointInfo,
-                fromBreakpoint: Boolean,
-                model: Listener4SpeedAssistExtend.Listener4SpeedModel
-            ) {
+        actionTv: TextView
+    ) {
+        var totalLength: Long = 0
+        var readableTotalLength: String? = null
+        task?.enqueue4WithSpeed(
+            onTaskStart = { statusTv.setText(R.string.task_start) },
+            onInfoReadyWithSpeed = { _, info, _, _ ->
                 statusTv.setText(R.string.info_ready)
                 totalLength = info.totalLength
                 readableTotalLength = Util.humanReadableBytes(totalLength, true)
                 DemoUtil.calcProgressToView(progressBar, info.totalOffset, totalLength)
-            }
-
-            override fun connectStart(
-                task: DownloadTask,
-                blockIndex: Int,
-                requestHeaders: Map<String, List<String>>
-            ) {
-                val status = "Connect Start $blockIndex"
-                statusTv.text = status
-            }
-
-            override fun connectEnd(
-                task: DownloadTask,
-                blockIndex: Int,
-                responseCode: Int,
-                responseHeaders: Map<String, List<String>>
-            ) {
+            },
+            onConnectStart = { _, blockIndex, _ ->
                 val status = "Connect End $blockIndex"
                 statusTv.text = status
-            }
-
-            override fun progressBlock(
-                task: DownloadTask,
-                blockIndex: Int,
-                currentBlockOffset: Long,
-                blockSpeed: SpeedCalculator
-            ) {
-            }
-
-            override fun progress(
-                task: DownloadTask,
-                currentOffset: Long,
-                taskSpeed: SpeedCalculator
-            ) {
+            },
+            onProgressWithSpeed = { _, currentOffset, taskSpeed ->
                 val readableOffset = Util.humanReadableBytes(currentOffset, true)
                 val progressStatus = "$readableOffset/$readableTotalLength"
                 val speed = taskSpeed.speed()
                 val progressStatusWithSpeed = "$progressStatus($speed)"
-
                 statusTv.text = progressStatusWithSpeed
                 DemoUtil.calcProgressToView(progressBar, currentOffset, totalLength)
             }
-
-            override fun blockEnd(
-                task: DownloadTask,
-                blockIndex: Int,
-                info: BlockInfo,
-                blockSpeed: SpeedCalculator
-            ) {
-            }
-
-            override fun taskEnd(
-                task: DownloadTask,
-                cause: EndCause,
-                realCause: Exception?,
-                taskSpeed: SpeedCalculator
-            ) {
-                val statusWithSpeed = cause.toString() + " " + taskSpeed.averageSpeed()
-                statusTv.text = statusWithSpeed
-                actionTv.setText(R.string.start)
-                // mark
-                task.tag = null
-                if (cause == EndCause.COMPLETED) {
-                    val realMd5 = fileToMD5(task.file!!.absolutePath)
-                    if (!realMd5!!.equals("f836a37a5eee5dec0611ce15a76e8fd5", ignoreCase = true)) {
-                        Log.e(TAG, "file is wrong because of md5 is wrong $realMd5")
-                    }
+        ) { task, cause, realCause, taskSpeed ->
+            val statusWithSpeed = cause.toString() + " " + taskSpeed.averageSpeed()
+            statusTv.text = statusWithSpeed
+            actionTv.setText(R.string.start)
+            // remove mark
+            task.tag = null
+            if (cause == EndCause.COMPLETED) {
+                val realMd5 = fileToMD5(task.file!!.absolutePath)
+                if (!realMd5!!.equals("f836a37a5eee5dec0611ce15a76e8fd5", ignoreCase = true)) {
+                    Log.e(TAG, "file is wrong because of md5 is wrong $realMd5")
                 }
             }
-        })
+            realCause?.let {
+                Log.e(TAG, "download error", it)
+            }
+        }
     }
 
     companion object {
@@ -251,12 +197,10 @@ class SingleActivity : BaseSampleActivity() {
         }
 
         @SuppressLint("DefaultLocale")
-        private fun convertHashToString(md5Bytes: ByteArray): String {
-            val buf = StringBuffer()
-            for (i in md5Bytes.indices) {
-                buf.append(((md5Bytes[i] and 0xff.toByte()) + 0x100).toString(16).substring(1))
+        private fun convertHashToString(md5Bytes: ByteArray): String = StringBuffer().apply {
+            md5Bytes.forEach { byte ->
+                append(((byte.toInt() and 0xff) + 0x100).toString(16).substring(1))
             }
-            return buf.toString().toUpperCase()
-        }
+        }.toString().toUpperCase()
     }
 }
